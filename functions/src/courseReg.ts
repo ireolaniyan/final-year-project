284b0b915a20c72a3066098ff7b0bef47b06f538
Ire Olaniyan <ireolaniyan7@gmail.com>
2018-09-05 05:48:34 +0200

    Added Course registration/edit info

---
+++ functions/src/courseReg.ts
@@ -0,0 +1,22 @@
+import firestore from './db';
+
+let courseRegDocRef = firestore.collection('courseRegistration').doc('courseRegData');
+
+export default async function courseRegistration(agent) {
+    let speech;
+    try {
+        const doc = await courseRegDocRef.get();
+        if (doc.exists) {
+            if (agent.parameters.course === 'course' && agent.parameters.registration === 'registration') {
+                speech = `You can register your courses between ${doc.data().courseReg.start} and ${doc.data().courseReg.end}`;
+            } else if (agent.parameters.course === 'course' && agent.parameters.edit === 'edit') {
+                speech = `That's from ${doc.data().courseEdit.start} to ${doc.data().courseEdit.end}. Hurry Up!`;
+            }
+            agent.add(speech);
+        } else {
+            console.log("Doc not found")
+        }
+    } catch (error) {
+        console.log("An error occured", error);
+    }
+}
